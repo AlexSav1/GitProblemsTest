a691d1ea107318f8296f37a46b30533cb0402ed3
Alex Laptop <savittalex@gmail.com>
2017-03-06 12:04:38 -0500

    init commit

---
+++ GitProblemsTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitProblemsTest
+//
+//  Created by Alex Laptop on 3/6/17.
+//  Copyright © 2017 Alex Laptop. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GitProblemsTest/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  GitProblemsTest
+//
+//  Created by Alex Laptop on 3/6/17.
+//  Copyright © 2017 Alex Laptop. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
